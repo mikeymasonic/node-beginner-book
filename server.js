0154fdb373c2b2a86d9d0475dac07bab3ee21ace
Michael Romay <amerror@gmail.com>
2020-03-22 12:08:41 -0700

    post data

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
-  const pathname = url.parse(request.url).pathname;
-  console.log("Request for " + pathname + " received.");
+    let postData = "";    
+    const pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
 
-  route(handle, pathname, response);
+  request.setEncoding("utf8");
+
+  request.addListener("data", function(postDataChunk) {
+    postData += postDataChunk;
+    console.log("Received POST data chunk '" +
+    postDataChunk + "'.");
+  });
+
+  requestAddListener("end", function() {
+    route(handle, pathname, response, postData);
+  });
+
 }
   http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
