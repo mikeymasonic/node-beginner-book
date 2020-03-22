2aceeca4db1a91610e54d610f3525ea8e7aae6e8
Michael Romay <amerror@gmail.com>
2020-03-22 11:23:39 -0700

    handlers

--- router.js
+++ router.js
@@ -1,5 +1,10 @@
 function route(pathname) {
   console.log("About to route a request for " + pathname);
+  if (typeof handle[pathname] === 'function'){
+    handle[pathname]();
+  } else {
+    console.log("No request handler found or " + pathname);
+  }
 }
 
 exports.route = route;
