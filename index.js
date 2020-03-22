6e2881c332dd334efd31c4234a6fe4310f8a6f1a
Michael Romay <amerror@gmail.com>
2020-03-22 11:29:52 -0700

    handlers fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./server");
 const router = require("./router");
 const requestHandlers = require("./requestHandlers");
 
-const handle = {};
+let handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
