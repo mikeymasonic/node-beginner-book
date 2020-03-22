405a1c8005a3cbbea7a3be23163080bf6cb0b70f
Michael Romay <amerror@gmail.com>
2020-03-22 11:01:59 -0700

    url parse

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const url = require("url");
 
 function start() {
   function onRequest(request, response) {
+  const pathname = url.parse(request.url).pathname;
   console.log("Request received.");
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello Travis");
