29f852a6c6d98a0aa845e1b1aa1fd6b10239a7fe
Michael Romay <amerror@gmail.com>
2020-03-22 11:44:40 -0700

    child process

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,20 @@
-function start() {
-console.log("Request handler 'start' was called.");
+const exec = require("child_process").exec;
+
+function start(response) {
+  console.log("Request handler 'start' was called.");
+  exec("ls -lah", function (error, stdout, stderr) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(stdout);
+    response.end();
+  });
 }
 
-function upload() {
+function upload(response) {
 console.log("Request handler 'upload' was called.");
+response.writeHead(200, {"Content-Type": "text/plain"});
+response.write("Hello Upload");
+response.end();
 }
 
-exports.start = start; exports.upload = upload;
+exports.start = start; 
+exports.upload = upload;
