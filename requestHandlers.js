ec5aeaaf7dec22256ccfd713cd804cb20beb530e
Michael Romay <amerror@gmail.com>
2020-03-22 12:11:48 -0700

    querystring

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,8 @@
 // const exec = require("child_process").exec;
 
-function start(response) {
+let querystring = require("querystring");
+
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
   let body =  '<html>'+
@@ -21,10 +23,10 @@ function start(response) {
       response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
 console.log("Request handler 'upload' was called.");
 response.writeHead(200, {"Content-Type": "text/plain"});
-response.write("Hello Upload");
+response.write("You've sent the text: "+ querystring.parse(postData).text);
 response.end();
 }
 
