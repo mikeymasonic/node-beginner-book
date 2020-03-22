7ecf81354bb5feb7f6919b1d80f41febd3054d65
Michael Romay <amerror@gmail.com>
2020-03-22 13:44:28 -0700

    all the things work now

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const server = require("./server");
-const router = require("./router");
-const requestHandlers = require("./requestHandlers");
+const router = require('./router');
+const requestHandlers = require('./requestHandlers');
 
-let handle = {};
-handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
-handle["/upload"] = requestHandlers.upload;
-handle["/show"] = requestHandlers.show;
+const handle = {};
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+handle['/show'] = requestHandlers.show;
 
 server.start(router.route, handle);
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const http = require("http");
 const url = require("url");
 
-function start(route, handle) {
-  function onRequest(request, response) {
-    const pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
+function start (route, handle) {
+function onRequest(request, response) {
+    let pathname = url.parse(request.url).pathname;
+    console.log('Request for' + pathname + ' received.');
     route(handle, pathname, response, request);
-  }
-  
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+}
+
+http.createServer(onRequest).listen(8888);
+console.log("Server has Started");
 }
 
 exports.start = start;
-
